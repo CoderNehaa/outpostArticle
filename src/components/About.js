2dd828bd9c8ec2766e91fa45c7133d14b54a0c07
Neha <nagdaneha97@gmail.com>
2023-12-20 21:00:01 +0530

    final commit

--- src/components/About.js
+++ src/components/About.js
@@ -2,12 +2,21 @@ import React from 'react';
 
 const About = () => {
 
-  function handliClick(addId, removeID){
+  function handleClick(addId, removeID){
     let removeHighlight = document.getElementById(removeID);
     removeHighlight.classList.remove("border-b-4", "border-violet-900", "text-violet-900");
 
     let addHighlight = document.getElementById(addId);
     addHighlight.classList.add("border-b-4", "border-violet-900", "text-violet-900");
+  }
+  
+  function handleBtnClick(id){
+    const btns = document.querySelector('#btns').querySelectorAll('button');
+    btns.forEach((btn) => {
+      btn.classList.remove('bg-violet-900', 'text-white', 'opacity-85');
+    })
+    const element = document.getElementById(id);
+    element.classList.add('bg-violet-900', 'text-white', 'opacity-85');
   }
 
   return (
@@ -25,16 +34,27 @@ const About = () => {
           </div>
 
         <div className='text-violet-900 font-medium py-4' id='btns'>
-          <button className='px-3 py-1 m-3 border-2 xl:px-6 xl:py-3 xl:rounded-lg hover:bg-violet-900 hover:text-white hover:opacity-85'> <i className="fa-regular fa-bookmark mr-3"></i> Like </button>
-          <button className='px-3 py-1 m-3 border-2 xl:px-6 xl:py-3 xl:rounded-lg hover:bg-violet-900 hover:text-white hover:opacity-85'> <i className="fa-solid fa-arrow-up-right-from-square mr-3"></i> Visit Site </button>
-          <button className='px-3 py-1 border-2 xl:px-6 xl:py-3 xl:rounded-lg hover:bg-violet-900 hover:text-white hover:opacity-85'> <i className="fa-solid fa-share-nodes mr-3"></i> Share </button>  
+          <button id='likeBtn' className='px-3 py-1 m-3 border-2 xl:px-6 xl:py-3 xl:rounded-lg bg-violet-900 text-white opacity-85' 
+              onClick={() =>handleBtnClick('likeBtn')}>
+              <i className="fa-regular fa-heart mr-3"></i> Like 
+          </button>
+
+          <button id='linkBtn' className='px-3 py-1 m-3 border-2 xl:px-6 xl:py-3 xl:rounded-lg'
+          onClick={() =>handleBtnClick('linkBtn')}>
+            <i className="fa-solid fa-arrow-up-right-from-square mr-3"></i> Visit Site 
+          </button>
+          
+          <button id='shareBtn' className='px-3 py-1 border-2 xl:px-6 xl:py-3 xl:rounded-lg '
+          onClick={() =>handleBtnClick('shareBtn')}>
+            <i className="fa-solid fa-share-nodes mr-3"></i> Share 
+          </button>  
         </div>
       </div>
 
       <div className='md:py-10 md:px-5 text-justify tracking-wide'>
         <div className='border-b-2 p-0'>
-          <button className='mr-8 md:text-2xl pb-2' id='overview' onClick={() => handliClick('overview', 'aboutOpenAI')}> Overview </button>
-          <button className='md:text-2xl pb-2' id='aboutOpenAI' onClick={() => handliClick('aboutOpenAI', 'overview')}> About Open AI </button>
+          <button className='mr-8 md:text-2xl pb-2' id='overview' onClick={() => handleClick('overview', 'aboutOpenAI')}> Overview </button>
+          <button className='md:text-2xl pb-2 border-b-4 border-violet-900 text-violet-900' id='aboutOpenAI' onClick={() => handleClick('aboutOpenAI', 'overview')}> About Open AI </button>
         </div>
         <p className='py-4 text-lg md:text-2xl text-slate-600'> OpenAI is an artificial intelligence research laboratory consisting of a team of world-class researchers and engineers focused on creating and advancing artificial intelligence in a safe and beneficial way for humanity. It was found in December 2015 by several high-profile individuals in the tech industry, including Elon Musk, Sam Altman, Greg Brockman, and others. </p>
         <p className='py-4 text-lg md:text-2xl text-slate-600'> OpenAI is dedicated to advancing the field of artificial intelligence through research. </p>
